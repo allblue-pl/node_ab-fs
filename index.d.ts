f219cd6064018de04bf1698c104fa809802ef47b
AllBlue Monkey <j.zolcik@allblue.pl>
2026-05-16 17:40:17 +0200

    Development.

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
-import abFS from "./lib/index.js";
+import abFS from "./ts-lib/index.js";
 import abFSMatcher from "ab-fs-matcher";
 import abFSWatcher from "ab-fs-watcher";
-import dir from "./lib/dir.js";
-import file from "./lib/file.js";
+import dir from "./ts-lib/dir.js";
+import file from "./ts-lib/file.js";
 
 export default abFS;
 export { abFSMatcher, abFSWatcher, dir, file };
